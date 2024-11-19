4f4285a4061d2f8355605002e89fab16b4878197
rpryor03 <rpryor03@hotmail.com>
2024-11-19 11:13:20 -0500

    Surfing Dismount Ground Effects

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -1680,6 +1680,7 @@ static void Task_WaitStopSurfing(u8 taskId)
         gPlayerAvatar.preventStep = FALSE;
         UnlockPlayerFieldControls();
         DestroySprite(&gSprites[playerObjEvent->fieldEffectSpriteId]);
+        playerObjEvent->triggerGroundEffectsOnMove = TRUE;
         DestroyTask(taskId);
     }
 }
